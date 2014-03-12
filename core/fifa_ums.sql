89d3dc5d84a0efc002c0158df230f549c672d118
wakhid <wakhid@uchiha>
2014-03-12 09:00:54 +0700

    update database

--- core/fifa_ums.sql
+++ core/fifa_ums.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 01, 2014 at 05:06 PM
+-- Generation Time: Mar 12, 2014 at 09:59 AM
 -- Server version: 5.5.35
--- PHP Version: 5.3.10-1ubuntu3.9
+-- PHP Version: 5.3.10-1ubuntu3.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,7 +31,14 @@ CREATE TABLE IF NOT EXISTS `informasi` (
   `judul` varchar(200) NOT NULL,
   `isi` text NOT NULL,
   PRIMARY KEY (`id_informasi`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `informasi`
+--
+
+INSERT INTO `informasi` (`id_informasi`, `judul`, `isi`) VALUES
+(1, 'Ultricies massa elementum', '<p><span style="font-family: arial; font-size: 13px;">Ultricies massa elementum dis mattis mauris porta, cum? Eros aliquam, tortor turpis egestas phasellus nunc enim! Rhoncus lorem cras, mattis et ultricies elit integer phasellus augue integer lorem, pulvinar? Ridiculus turpis et ut a, rhoncus, augue? Elementum platea? Eu tempor? Eu porta. Proin vut, cum tincidunt adipiscing pid. Mauris dictumst ac facilisis. Cum urna, elementum nunc turpis a natoque cras? Natoque cras, lorem ac! Velit odio etiam mattis augue in ac porttitor, proin adipiscing? Porttitor turpis eros pulvinar? Elementum nunc egestas proin nisi dolor, ut, tortor pulvinar risus, sed magna? Penatibus eros odio! Magnis mus! Tortor? Aliquam vut, nisi. Pellentesque amet sit duis arcu sagittis! Turpis, in scelerisque, aliquam turpis porttitor, hac, lectus elementum ultrices non cras! Ac magna, ultrices.</span></p>');
 
 -- --------------------------------------------------------
 
@@ -67,7 +74,16 @@ CREATE TABLE IF NOT EXISTS `jabatan` (
   `id_jabatan` int(11) NOT NULL AUTO_INCREMENT,
   `jabatan` varchar(11) NOT NULL,
   PRIMARY KEY (`id_jabatan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `jabatan`) VALUES
+(1, 'Admin'),
+(2, 'Bagmawa'),
+(3, 'Ormawa');
 
 -- --------------------------------------------------------
 
@@ -79,7 +95,14 @@ CREATE TABLE IF NOT EXISTS `jenis_dana` (
   `id_jenis` int(11) NOT NULL AUTO_INCREMENT,
   `nama_jenis` varchar(145) NOT NULL,
   PRIMARY KEY (`id_jenis`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `jenis_dana`
+--
+
+INSERT INTO `jenis_dana` (`id_jenis`, `nama_jenis`) VALUES
+(1, 'Dana Operasional');
 
 -- --------------------------------------------------------
 
@@ -107,6 +130,24 @@ CREATE TABLE IF NOT EXISTS `kelola_dana` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `link`
+--
+
+CREATE TABLE IF NOT EXISTS `link` (
+  `name` varchar(100) NOT NULL,
+  `url` varchar(145) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `link`
+--
+
+INSERT INTO `link` (`name`, `url`) VALUES
+('site', 'http://localhost/fifa');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `proker`
 --
 
@@ -115,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `proker` (
   `id_user` int(11) NOT NULL,
   `nama_file` varchar(145) NOT NULL,
   `tgl_proker` date NOT NULL,
-  `status` varchar(45) NOT NULL,
+  `status` enum('setuju','tidak') NOT NULL,
   PRIMARY KEY (`id_proker`),
   KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -133,7 +174,16 @@ CREATE TABLE IF NOT EXISTS `sub_dana` (
   PRIMARY KEY (`id_sub`),
   KEY `id_jenis` (`id_jenis`),
   KEY `id_jenis_2` (`id_jenis`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `sub_dana`
+--
+
+INSERT INTO `sub_dana` (`id_sub`, `nama_sub`, `id_jenis`) VALUES
+(1, 'Nalar', 1),
+(2, 'Non Nalar', 1),
+(3, 'Reor', 1);
 
 -- --------------------------------------------------------
 
@@ -147,14 +197,27 @@ CREATE TABLE IF NOT EXISTS `total_dana` (
   `id_jenis` int(11) NOT NULL,
   `id_sub` int(11) NOT NULL,
   `total_dana` int(11) NOT NULL,
-  `tgl_total_dana` date NOT NULL,
+  `tgl_total_dana` varchar(20) NOT NULL,
+  `tahun` year(4) NOT NULL,
   PRIMARY KEY (`id_total_dana`),
   KEY `id_user` (`id_user`,`id_jenis`,`id_sub`),
   KEY `id_user_2` (`id_user`),
   KEY `id_sub` (`id_sub`),
   KEY `total_dana` (`total_dana`),
   KEY `id_jenis` (`id_jenis`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=55 ;
+
+--
+-- Dumping data for table `total_dana`
+--
+
+INSERT INTO `total_dana` (`id_total_dana`, `id_user`, `id_jenis`, `id_sub`, `total_dana`, `tgl_total_dana`, `tahun`) VALUES
+(43, 3, 1, 1, 764764, '11/03', 2013),
+(44, 3, 1, 2, 574869, '11/03', 2013),
+(45, 3, 1, 3, 6536789, '11/03', 2013),
+(52, 3, 1, 1, 9780000, '11/03', 2014),
+(53, 3, 1, 2, 96968987, '11/03', 2014),
+(54, 3, 1, 3, 32499690, '11/03', 2014);
 
 -- --------------------------------------------------------
 
@@ -165,12 +228,22 @@ CREATE TABLE IF NOT EXISTS `total_dana` (
 CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `password` varchar(145) NOT NULL,
   `nama` varchar(145) NOT NULL,
   `id_jabatan` int(11) NOT NULL,
   PRIMARY KEY (`id_user`),
   KEY `id_jabatan` (`id_jabatan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `id_jabatan`) VALUES
+(1, 'admin', 'MjEyMzJmMjk3YTU3YTVhNzQzODk0YTBlNGE4MDFmYzMvYWRtaW4=', 'Administrator', 1),
+(2, 'bagmawa', 'NzY1YzBmMDBkNGIyYzU2Mjg2MTkyODNiMGM4NWYzNWYvYmFnbWF3YQ==', 'Bagmawa', 2),
+(3, 'ormawa', 'MzY1NWRiMzY2OGMyYzUxYTcxODRhYzFhNTU2NjA1OGYvb3JtYXdh', 'Ormawa', 3),
+(8, 'bem', 'ZDNjNjU0ZDk5YmRmYWYxMDFlMDEyYmZlMjgxMDY3OWUvYmVt', 'BEM FKI', 3);
 
 --
 -- Constraints for dumped tables
